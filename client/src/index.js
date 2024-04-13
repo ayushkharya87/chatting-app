7c30d1c7ae6321c4c86d4d994e4b387594081e23
ayush <ayush87269@gmail.com>
2024-04-13 15:06:32 +0530

    add vercel

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ import { persistStore } from 'redux-persist';
 let persistor = persistStore(store);
 
 // export const BASE_URL="http://localhost:8080"
-export const BASE_URL="https://chatting-app-ruddy.vercel.app/"
+export const BASE_URL="https://chatting-app-lake.vercel.app"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
