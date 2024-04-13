40d6a0a7e11c85c9050eb9302dd9f50088593217
ayush <ayush87269@gmail.com>
2024-04-13 23:15:27 +0530

    update

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import { persistStore } from 'redux-persist';
 let persistor = persistStore(store);
 
 // export const BASE_URL="http://localhost:8080"
-export const BASE_URL=`https://chatting-app-topaz.vercel.app/`
+export const BASE_URL=`https://chatting-app-topaz.vercel.app`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
